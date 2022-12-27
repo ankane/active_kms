750ab459489c52eb0cab9580028b5af4e92f2786
Andrew Kane <andrew@ankane.org>
2022-12-27 15:05:18 -0800

    Improved tests

--- test/key_provider_test.rb
+++ test/key_provider_test.rb
@@ -5,13 +5,36 @@ class KeyProviderTest < Minitest::Test
     User.delete_all
   end
 
-  def test_works
+  def test_create
     # should ideally be encrypt: 1 and no decrypt
     # https://github.com/rails/rails/issues/42388
     encrypt = ActiveRecord::VERSION::STRING.to_f >= 7.1 ? 1 : 3
     assert_operations encrypt: encrypt, decrypt: 1 do
       User.create!(email: "test@example.org")
     end
+  end
+
+  def test_assign
+    user = User.new
+
+    assert_operations do
+      user.email = "test@example.org"
+    end
+  end
+
+  def test_update
+    user = User.create!(email: "test@example.org")
+
+    # should ideally be encrypt: 1 and no decrypt
+    # https://github.com/rails/rails/issues/42388
+    encrypt = ActiveRecord::VERSION::STRING.to_f >= 7.1 ? 1 : 3
+    assert_operations encrypt: encrypt, decrypt: 3 do
+      user.update!(email: "test2@example.org")
+    end
+  end
+
+  def test_query
+    User.create!(email: "test@example.org")
 
     user =
       assert_operations do
